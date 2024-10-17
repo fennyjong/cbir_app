931244da6e9f686e3594c512ec93c6b867977afd
fennyjong <fennyjong7@gmail.com>
2024-10-17 18:16:51 +0700

    oke

---
+++ query_db.sql
@@ -0,0 +1,13 @@
+SELECT * FROM label
+SELECT * FROM songket_dataset
+select * from "user"
+
+
+DELETE FROM label
+DELETE FROM songket_dataset
+DELETE FROM "user"
+
+DROP TABLE label
+DROP TABLE "user";
+DROP TABLE songket_dataset
+
